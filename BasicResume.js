0e1a74ad3854f84fdec5bc70c2ffdc1f4652e902
pranshu jain <pranshujain@MacBook-Pro.local>
2023-07-17 18:41:00 +0530

    first commit

---
+++ BasicResume.js
@@ -0,0 +1,46 @@
+const express= require("express");
+// quotes around "express" ignify that "express" is a string representing the name of the module to be imported.
+const app= express();
+// Calling express function
+
+
+app.get("/",(req,res )=>{
+    // app.get takes 2 arguments ...first - "/"- this tells the location where we have to render. ("/" this is for the home page)
+// second-callback fn tells the sever what to do when the request happens
+ // you can write anything in place of request and response . generally we wite req, res.
+
+
+    // console.log(request);
+    // used to log the request object to the console. It is a way to inspect the details of the incoming HTTP request made to your server.
+
+    res.send(`Hello, I am Pranshu Jain. Welcome to my Resume`);
+    // sends this response to website
+   
+
+});
+app.get("/MySkills", (req, res)=>{
+ res.send(`SKILLS
+ <br>
+    Languages- Java, JavaScript
+    <br>
+    Frontend- HTML/C SS, React.js<br>
+    Backend- Node.js, SQL<br>
+    Core Subject- Data Structures and Algorithms<br>
+    Tools- Git, GitHub, MS Excel, Power BI<br>
+    Behavioral-Teamwork, Networking skills, Management, Public speaking, People skills, Leadership<br>
+    `)
+})
+
+app.get("/Interests", (req, res)=>{
+    res.send(`INTERESTS<br>
+    <br>
+    Playing Football <br> Yoga<br>
+    Martial Arts <br>Writing Poems
+    <br> Volunteering<br> UCMAS<br>
+    Dance <br>Reading books`)
+})
+
+
+app.listen(3000,()=>console.log("listening to port 3000"));
+// browser send request to google servers..it will send browser a response (contains js , html, css to render the website)
+
